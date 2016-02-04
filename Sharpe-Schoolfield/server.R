1c6f870ffb359f94c685953fcbfbd6dd6fd6a6d8
padpadpadpad <d.padfieldscfc@gmail.com>
2016-02-04 21:53:45 +0000

    changes to app

--- Sharpe-Schoolfield/server.R
+++ Sharpe-Schoolfield/server.R
@@ -9,17 +9,38 @@ library(shiny)
 library(TeamPhytoplankton)
 library(ggplot2)
 
-temperature <- seq(0,100,0.1)
-
 shinyServer(function(input, output) {
   
   # create output graph
-  output$SHplot <- renderPlot({
-    ggplot() +
-      geom_line(aes(temperature, schoolfield.high(input$`b(Tc)`, input$Ea, input$Eh, input$Th + 273.15, temp = temperature + 273.15, Tc = input$Tc)), linetype = 2, col = 'red', size = 3) +
+  output$plot1 <- renderPlot({
+    
+    plot1.data <- data.frame(expand.grid(temperature = seq(input$temp[1], input$temp[2], 0.1), curve = c('High inactivation only', 'Full model')))
+    
+    plot1.data$y <- NA
+    
+    plot1.data$y[plot1.data$curve == 'High inactivation only'] <- schoolfield.high(input$`b(Tc)`, input$Ea, input$Eh, input$Th + 273.15, temp = plot1.data$temperature[plot1.data$curve == 'High inactivation only'] + 273.15, Tc = input$Tc)
+    plot1.data$y[plot1.data$curve == 'Full model'] <- schoolfield(input$`b(Tc)2`, input$Ea2, input$El, input$Tl + 273.15, input$Eh2, input$Th2 + 273.15, temp = plot1.data$temperature[plot1.data$curve == 'Full model'] + 273.15, Tc = input$Tc2)
+    
+    head(plot1.data)
+    
+     plot1 <-  ggplot(plot1.data[plot1.data$curve %in% input$Curve,],
+                      aes(x = temperature, y = y, group = curve)) +
+      geom_line(aes(col = curve), linetype = 1, size = 1.5) +
       theme_classic(base_size = 18, base_family = 'Helvetica') +
       xlab('Temperature (ºC)') +
-      ylab('ln Rate')
+      ylab('ln Rate') 
+
+    print(plot1)
+  })
+
+##############################################################################################
+  # create output table for parameters used
+  sliderValues <- reactive({
+   data.frame(bTc = input$`b(Tc)`, Tc = input$Tc, Ea = input$Ea, Eh = input$Eh, Th = input$Th, row.names = NULL)
+  })
+  
+  output$values <- renderTable({
+   sliderValues()
   })
 
 })
--- Sharpe-Schoolfield/ui.R
+++ Sharpe-Schoolfield/ui.R
@@ -10,46 +10,104 @@ library(shiny)
 shinyUI(fluidPage(
 
   # Application title
-  titlePanel("Modified Sharpe-Schoolfield Thermal Response Curves"),
+  headerPanel("Modified Sharpe-Schoolfield Thermal Response Curves"),
 
   # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel('Parameters:',
-      sliderInput("b(Tc)",
+
+    fluidRow(
+      column(2,
+          
+          h3('Schoolfield high parameters'),
+      
+          sliderInput("b(Tc)",
                   "b(Tc):",
                   min = -10,
                   max = 10,
                   value = 0,
                   step = 0.1),
-      sliderInput("Tc",
+          sliderInput("Tc",
                   "Tc:",
                   min = 0,
                   max = 100,
-                  value = 50,
-                  step = 5),
-      sliderInput("Ea",
+                  value = 20,
+                  step = 1),
+          sliderInput("Ea",
                   "Ea:",
                   min = 0,
                   max = 2,
-                  value = 1,
+                  value = 0.6,
                   step = 0.1),
-      sliderInput("Eh",
+          sliderInput("Eh",
                   "Eh:",
                   min = 0,
                   max = 10,
-                  value = 5,
-                  step = 0.5),
-      sliderInput("Th",
+                  value = 4,
+                  step = 0.1),
+          sliderInput("Th",
                   "Th:",
                   min = 0,
                   max = 100,
-                  value = 50,
-                  step = 5)
-    ),
+                  value = 40,
+                  step = 1)
+      ),
+      column(2,
+             
+             h3('Full model parameters'),
+             
+             sliderInput("b(Tc)2",
+                         "b(Tc):",
+                         min = -10,
+                         max = 10,
+                         value = 0,
+                         step = 0.1),
+             sliderInput("Tc2",
+                         "Tc:",
+                         min = 0,
+                         max = 100,
+                         value = 20,
+                         step = 1),
+             sliderInput("Ea2",
+                         "Ea:",
+                         min = 0,
+                         max = 2,
+                         value = 0.6,
+                         step = 0.1),
+             sliderInput("El",
+                         "El:",
+                         min = 0,
+                         max = 10,
+                         value = 4,
+                         step = 0.1),
+             sliderInput("Tl",
+                         "Tl:",
+                         min = 0,
+                         max = 100,
+                         value = 15,
+                         step = 1),
+             sliderInput("Eh2",
+                         "Eh:",
+                         min = 0,
+                         max = 10,
+                         value = 4,
+                         step = 0.1),
+             sliderInput("Th2",
+                         "Th:",
+                         min = 0,
+                         max = 100,
+                         value = 40,
+                         step = 1)
+      ),
+      column(8,
+             checkboxGroupInput('Curve', 'Curve:', choices = c("High inactivation only", "Full model"), selected = NULL, inline = TRUE),
+             sliderInput("temp",
+                         "Temperature ºC:",
+                         min = 0,
+                         max = 100,
+                         value = c(10, 50),
+                         step = 1),
+             plotOutput("plot1"),
+             tableOutput("values"))
 
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("SHplot")
     )
-  )
+
 ))
